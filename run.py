6d185c53d88ed56de9f190532d30a022875662bf
Alina-Zhi <xzhi@ucsd.edu>
2023-03-14 21:07:01 -0700

    Update run.py

--- run.py
+++ run.py
@@ -28,9 +28,11 @@ from src.background import *
 from src.rgb_visualization import *
 from src.location_group import *
 from src.CNNmodel import *
+from src.normalize import *
+from src.iwildcam_dataset import *
 
 
-def main(trail = 'main', batchsize=64):
+def main(trail = 'main', batchsize=64, epoch = 10, bg_remove = False, normalize = False):
     """
     trails: main / test
     Train: trian new model / read exist model
@@ -38,16 +40,24 @@ def main(trail = 'main', batchsize=64):
     """
    
     #initialize model
-    epoch = 100
-    model=CustomCNN(500) #input: Final Class Labels
+    epoch = epoch
+    model=CustomCNN(2) #input: Final Class Labels
     model.to(model.device)
     
+    #Keep Record
     mean_losses=[]
+    mean_losses_val=[]
+    accuracy=[]
+    accuracy_val=[]
+    best_val_acc = 0
+    min_val_loss = 1000
+    
     #Get Data Loader Based on Trail
     if trail == 'main':
-        dataset = get_dataset(dataset="iwildcam", download=False) #size 203029
+        #dataset = get_dataset(dataset="iwildcam", download=False) #size 203029
+        dataset=IWildCamDataset_custom(root_dir='data', download=False, split_scheme='official')
         train_data = dataset.get_subset(
-            "id_val",
+            "train", #'train'
             transform=transforms.Compose(
                 [transforms.Resize((448, 448)), transforms.ToTensor()]
             ),)
@@ -55,21 +65,92 @@ def main(trail = 'main', batchsize=64):
         #train_loader = DataLoader(train_data, batch_size=batchsize, shuffle=False)
         print('Train Data')
     elif trail == 'test':
-        img,ids=get_imgs("data/raw/sample/", "data/raw/metadata_test.csv")
-        dataset = [img,ids]
+        img,ids,info=get_imgs("data/raw/sample/", "data/raw/metadata_test.csv")
+        dataset = [img,ids,info]
         #gen=data_test_generator(img, ids, 32)
         print('Test Trail Data')
         
     #Train Model For loop train model for given eqpoch
     for ep in range(epoch):
-        gen=get_data_generator(trail, dataset, batchsize = batchsize, shuffle=False)
-        r=train_oneepoch(gen, model, 'entropy', optimizer ='Adam', learning_rate = 0.00001, bg_remove = True)
-        #print(r) #r: average loss over a epoch
-        mean_losses+=[r]
-        #validate_oneepoch(data_loader, model, loss_func, optimizer = None, learning_rate = 0.0001)
-    
+        gen=get_data_generator(trail, dataset, batchsize = batchsize, shuffle=True)
+        epoch_loss=train_oneepoch(gen, model, 'entropy', optimizer ='Adam', learning_rate = 0.0001, bg_remove = bg_remove,normalize = normalize)
+        print(epoch_loss)
+        mean_losses+=[epoch_loss]
+        
+#         gen=get_data_generator(trail, dataset, batchsize = batchsize, shuffle=False)
+#         epoch_loss_val=validation_oneepoch(gen, model, 'entropy', optimizer =None, learning_rate = 0.00001, bg_remove = bg_remove,normalize = normalize)
+#         mean_losses_val+=[epoch_loss_val]
+        
+        if epoch_loss < min_val_loss:
+            min_val_loss = epoch_loss
+            model_dict = model.state_dict()
+            #state_dict = {'model': model_dict} #'optimizer': self.__optimizer.state_dict()}
+            
+        
+#         if epoch_loss_val < min_val_loss:
+#             min_val_loss = epoch_loss_val
+#             mode_savepath = f"best_model_{str(datetime.today())[:10].replace(' ','_')}.pt"
+#             model_dict = model.state_dict()
+#             state_dict = {'model': model_dict} #'optimizer': self.__optimizer.state_dict()}
+#             torch.save(state_dict, mode_savepath)
+            #If load Model:
+            #model = CustomCNN(100)
+            #model.load_state_dict(torch.load(PATH))
     #Display Output
-    return mean_losses
+    #Save the best model
+    mode_savepath = f"best_model_{str(datetime.today())[:10].replace(' ','_')}.pt"
+    torch.save(model_dict, mode_savepath)
+    return [mean_losses, mean_losses_val, accuracy, accuracy_val]
+
+def Alex(trail = 'alex', batchsize=64, epoch = 10, bg_remove = False, normalize = False):
+    """
+    trails: main / test
+    Train: trian new model / read exist model
+    experiment name: BG sub, OT
+    """
+   
+    #initialize model
+    epoch = epoch
+    model=TransferAlex(50) #input: Final Class Labels
+    model.model.to(model.device)
+    
+    #Keep Record
+    mean_losses=[]
+    mean_losses_val=[]
+    accuracy=[]
+    accuracy_val=[]
+    best_val_acc = 0
+    min_val_loss = 1000
+    
+    #Get Data Loader Based on Trail
+    dataset=IWildCamDataset_custom(root_dir='data', download=False, split_scheme='official')
+    train_data = dataset.get_subset(
+        "train", #'train'
+        transform=transforms.Compose(
+            [transforms.Resize((448, 448)), transforms.ToTensor()]
+        ),)
+    dataset=train_data
+
+        
+    #Train Model For loop train model for given eqpoch
+    for ep in range(epoch):
+        gen=get_data_generator(trail, dataset, batchsize = batchsize, shuffle=True)
+        epoch_loss=train_oneepoch_alex(gen, model, 'entropy', optimizer ='SGD', learning_rate = 0.001, bg_remove = bg_remove,normalize = normalize)
+        print(epoch_loss)
+        mean_losses+=[epoch_loss]
+
+        
+        if epoch_loss < min_val_loss:
+            min_val_loss = epoch_loss
+            model_dict = model.model.state_dict()
+            #Save the best model
+            mode_savepath = f"best_model_{str(datetime.today())[:10].replace(' ','_')}.pt"
+            torch.save(model_dict, mode_savepath)
+            #state_dict = {'model': model_dict} #'optimizer': self.__optimizer.state_dict()}
+            
+    #Display Output
+    
+    return [mean_losses, mean_losses_val, accuracy, accuracy_val]
 
 def get_imgs(test_data_path, test_meta_path):
     
@@ -88,6 +169,7 @@ def get_imgs(test_data_path, test_meta_path):
     category_ids=metadata_test['category_id'].values
     images=[]
     labels=[]
+    info=[]
     for i, f in enumerate(filenames):
         try:
             img=plt.imread("data/raw/sample/"+f) #read the image
@@ -97,48 +179,65 @@ def get_imgs(test_data_path, test_meta_path):
         else:
             images+=[img[:,:448,:448]] #crop to 3x448x448
             labels+=[category_ids[i]]
+            info+=[metadata_test.iloc[i]['location']]
         if i==200:
             break
     images=torch.from_numpy(np.array(images)).float()
     labels=torch.from_numpy(np.array(labels))#.float()
+    infos=torch.from_numpy(np.array(info))
     print(images.shape,labels.shape)
     
-    return images, labels
+    return images, labels,infos
 
-def data_test_generator(img, labels, batchsize):
+def data_test_generator(dataset, batchsize):
+    img, labels,info=dataset
     for i in range(0, len(img), batchsize):
-        if i + batchsize<len(img):
-            yield img[i:i + batchsize], labels[i:i + batchsize]
+        if (i + batchsize)<len(img):
+            yield img[i:i + batchsize], labels[i:i + batchsize],info[i:i + batchsize]
             
-def get_data_generator(trail, dataset, batchsize = 32, shuffle=False):
+def get_data_generator(trail, dataset, batchsize = 64, shuffle=False):
     if trail == 'main':
         gen = DataLoader(dataset, batch_size=batchsize, shuffle=shuffle)
         #print('Train Data')
+    if trail == 'alex':
+        gen = DataLoader(dataset, batch_size=batchsize, shuffle=shuffle)
+        #print('Train Data')
     elif trail == 'test':
-        gen=data_test_generator(dataset[0], dataset[1], batchsize)
+        gen=data_test_generator(dataset, batchsize)
         #print('Test Trail Data')
     return gen
 
-def draw_loss_plot(losses, save_path='result/'):
-    plt.plot(losses)
+def draw_loss_plot(results, save_path='result/'):
+    for i in results:
+        plt.plot(i)
     plt.xlabel('epoch count')
-    plt.ylabel('loss')
-    plt.title('Training Loss over epochs')
+    plt.ylabel('loss & Accuracy')
+    plt.title('loss & Accuracy over epochs')
     save_file_path = save_path+f"loss_plot_{str(datetime.today())[:19].replace(' ','_')}.png"
     plt.savefig(save_file_path)
     print(f'save the loss image to {save_file_path}')
 
 if __name__ == '__main__': #if run from command line
     targets = sys.argv[1:]
+    if len(targets)>1:
+        epoch = int(targets[1]);print(epoch)
+    else:
+        epoch =10
     if targets[0] =='main':
-        losses=main('main',batchsize=64)
+        #Main trail
+    #if len(targets)>2:
+        results=main('main',batchsize=64,epoch = epoch,bg_remove = True, normalize = True)
     if targets[0] =='test':
-        losses=main('test',batchsize=32)
-    
+        #Test Trail
+        results=main('test',batchsize=32,epoch = epoch)
+    if targets[0] =='alex':
+        #alexnet Trail
+        results=Alex('alex',batchsize=32,epoch = epoch,bg_remove = True)
+    #NOTE: Results: = [mean_losses, mean_losses_val, accuracy, accuracy_val]
     #To Save Result:
     path = 'result/'
     #Check and Create dir for result
     if not os.path.exists(path):
         os.mkdir(path)
     if os.path.exists(path):
-        draw_loss_plot(losses, save_path='result/')
+        draw_loss_plot(results, save_path='result/')
